4f0c26a537853c9e91a92f9a64495419c8d37bd5
tk-0311 <113018868+tk-0311@users.noreply.github.com>
2023-12-01 21:21:18 -0500

    finished error free index.cpp file

--- LinkedList/index.cpp
+++ LinkedList/index.cpp
@@ -90,93 +90,133 @@ void LinkedList::Add(){
   Node* newNode = new Node(id,gpa,name);
   if (head == nullptr) {
     head = newNode;
+    cout << "\033[32mNew list is added.\033[0m" << endl;
   }else {
     Node* cur = head;
-    while (cur->next != nullptr){
-      cur = cur->next;
+    if (head->ID > newNode->ID) {
+      head = newNode;
+      newNode->next = cur;
+    }else {
+      while (cur->next != nullptr){
+        if (cur->next->ID > newNode->ID) {
+          break;
+        }
+        cur = cur->next;
+      }
+      if (cur->ID == newNode->ID) {
+        cout << "\033[31mDuplicate ID found. Please enter other ID \033[0m" << endl;
+        delete newNode;
+      }else {
+        Node* temp = cur->next;
+        cur->next = newNode;
+        newNode->next = temp;
+        cout << "\033[32mNew list is added.\033[0m" << endl;
+      }
+      
     }
-    cur->next = newNode;
+    
   }
+  
 };
 void LinkedList::Delete(){
-  int id = validateID();
+  
   if (head == nullptr) {
-      cout << "\033[31mLinked List is Empty!\033[0m";
-  }else if (head->next != nullptr) {
-    Node* cur = head;
-    while(cur->next != nullptr){
-      if (cur->next->ID == id) {
-        break;
-      }
-      cur = cur->next;
-    };
-    Node* temp1 = cur->next;
-    Node* temp2 = temp1->next;
-    delete temp1;
-    cur->next = temp2;
-  }else {
+      cout << "\033[31mLinked List is Empty!\033[0m" << endl;
+  }else{
+    Display();
+    int id = validateID();
+    if (head->ID == id){
       Node* temp = head;
+      Node* next = head->next;
       delete temp;
-      head = nullptr;
+      head = next;
+      cout << "\033[32mList is deleted\033[0m" << endl;
+    }else{ 
+      Node* prev = head;
+      Node* cur = prev->next;
+      while(cur->next != nullptr){
+        if (cur->ID == id) {
+          break;
+        }
+        prev = prev->next;
+        cur= cur->next;
+      };
+      if (cur->ID != id) {
+        cout << "\033[31mNo Matching ID found!\033[0m" << endl;
+      }else {
+          Node* temp1 = cur;
+          cur = cur->next;
+          delete temp1;
+          prev->next = cur;
+          cout << "\033[32mList is deleted\033[0m" << endl;
+      }
+    }
   }
 };
 void LinkedList::Display(){
   if (head == nullptr) {
-    cout << "\033[31mLinked List is Empty!\033[0m";
+    cout << "\033[31mLinked List is Empty!\033[0m" << endl;
   }else {
     Node* cur = head;
     while (cur != nullptr) {
-      cout << "ID: " << cur->ID << " | GPA: " << cur->GPA << " | NAME: " << cur->NAME << endl;
+      cout << "\033[32mID: " << cur->ID << " | GPA: " << cur->GPA << " | NAME: " << cur->NAME << "\033[0m" <<endl;
       cur = cur->next;
     }
   }
 };
 void LinkedList::Modify(){
   if (head == nullptr) {
-    cout << "\033[31mLinked List is Empty!\033[0m";
+    cout << "\033[31mLinked List is Empty!\033[0m" << endl;
   }else {
     int id = validateID();
     double gpa = validateGPA();
     string name = validateName();
     Node* cur = head;
-    while (cur == nullptr) {
+    while (cur != nullptr) {
       if (cur->ID == id) {
-        cout << "We found matching ID." << endl << "ID: " << cur->ID << " | GPA: " << cur->GPA << " | NAME: " << cur->NAME << endl << endl;
+        cout << "\033[32mWe found matching ID." << endl << "ID: " << cur->ID << " | GPA: " << cur->GPA << " | NAME: " << cur->NAME << endl << endl;
         cur->GPA = gpa;
         cur->NAME= name;
-        cout << "ID: " << cur->ID << " | GPA: " << cur->GPA << " | NAME: " << cur->NAME << endl;
+        cout << "ID: " << cur->ID << " | GPA: " << cur->GPA << " | NAME: " << cur->NAME << "\033[0m" << endl;
         break;
+      }else {
+        cur = cur->next;
       }
     }
-    cur == nullptr ? cout << "ID was not found": cout << endl;
+    cur == nullptr ? cout << "\033[31mID was not found\033[0m" << endl : cout << endl;
   }
 };
 void LinkedList::Purge(){
   if (head == nullptr) {
-    cout << "\033[31mLinked List is Empty!\033[0m";
+    cout << "\033[31mLinked List is Empty!\033[0m" << endl;
   }else {
     Node* cur = head;
     while (cur != nullptr) {
     Node* temp = cur;
     cur = cur->next;
     delete temp;
+    cout << "\033[31mList is deleted.\n";
   }
+  head = nullptr;
+  cout << "\033[32mWe successfully purged the linked list!\033[0m" << endl;
   }
 };
 void LinkedList::Search(){
  if (head == nullptr) {
-    cout << "\033[31mLinked List is Empty!\033[0m";
+    cout << "\033[31mLinked List is Empty!\033[0m" << endl;
   }else {
     int id = validateID();
     Node* cur = head;
-    while (cur == nullptr) {
+    while (cur != nullptr) {
       if (cur->ID == id) {
-        cout << "We found matching ID." << endl;
-        cout << "ID: " << cur->ID << " | GPA: " << cur->GPA << " | NAME: " << cur->NAME << endl;
+        cout << "\033[32mWe found matching ID." << endl;
+        cout << "ID: " << cur->ID << " | GPA: " << cur->GPA << " | NAME: " << cur->NAME << "\033[0m" << endl;
         break;
+      }else {
+        cur = cur->next;
       }
     }
-    cur == nullptr ? cout << "ID was not found": cout << endl;
+    cur == nullptr ? cout << "\033[31mID was not found\033[0m" <<endl: cout << endl;
   }
 };
 
